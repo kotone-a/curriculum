2ba538652c39873d5bb62b3cb7887d52b954e773
kotone-a <kotoyagi12@gmail.com>
2021-07-11 00:02:55 +0900

    提出課題
    Check.java
    Pet.java
    Constants.java

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,107 @@
+package check;
+
+
+public class Check{
+
+public static void main(String[] args) { 
+  
+
+    String firstName = "青柳";
+    String lastName = "琴音";
+
+    System.out.println("printNameメソッド→" + printName(firstName,lastName));
+
+
+
+
+        Pet a = new Pet("java吉","hoge");
+        a.introduce();
+        RobotPet b = new RobotPet("R2D2","ルーク");
+        b.introduce();
+   
+}
+
+
+
+       private static  String printName (String firstName, String lastName) {
+           return "青柳"+ "琴音";
+
+           
+       
+  
+        }
+
+}
+
+   
+  
+          
+    
+      
+  
+   
+
+
+    
+    
+   
+    
+
+   
+    
+   
+   
+
+    
+
+
+
+    
+
+
+
+
+
+    
+    
+    
+
+
+
+    
+
+    
+    
+   
+
+
+
+
+
+
+
+
+           
+        
+
+
+           
+    
+
+
+
+
+
+   
+
+
+   
+
+
+
+   
+    
+
+
+
+
